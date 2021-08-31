4329d997818ef85b553eb5c300d353e952fba25f
xilvia <kapuvari.szilvia@gmail.com>
2021-08-31 20:01:33 +0200

    minor changes

--- docs/main.js
+++ docs/main.js
@@ -1,13 +1,13 @@
-
-
 const solver = new SudokuSolver();
-var time = 0;
 
+var time = 0;
+var array = [];
+var stepCounter;
 
 function solve() {
-  var s = '';
-  for (var i = 0; i < 81; ++i) {
-    var y = document.getElementById('C' + i).value;
+  let s = '';
+  for (let i = 0; i < 81; ++i) {
+    let y = document.getElementById('C' + i).value;
     if (y >= 1 && y <= 9) {
       s += '' + y;
     } else {
@@ -15,26 +15,25 @@ function solve() {
     }
   }
 
-  var time_beg = new Date().getTime();
-  var x = solver.solve(s);
-
-  var t = (new Date().getTime() - time_beg) / 1000.0;
+  let time_beg = new Date().getTime();
+  let x = solver.solve(s);
+  let t = (new Date().getTime() - time_beg) / 1000.0;
 
   document.getElementById('runtime').innerHTML = 'Solved puzzle in ' + t + ' seconds ( ' + t * 1000.0 + ' ms ).';
   s = '';
 
-  for (var z = 0; z < 81; ++z) {
+  for (let z = 0; z < 81; ++z) {
     document.getElementById('C' + z).value = x[z];
   }
 }
 
 function set_9x9(str) {
-  console.log(str)
+  displayChangesInArray(str)
   if (str != null && str.length >= 81) {
-    for (var i = 0; i < 81; ++i) {
+    for (let i = 0; i < 81; ++i) {
       document.getElementById('C' + i).value = '';
     }
-    for (var j = 0; j < 81; ++j) {
+    for (let j = 0; j < 81; ++j) {
       if (str.substr(j, 1) >= 1 && str.substr(j, 1) <= 9) {
         document.getElementById('C' + j).value = str.substr(j, 1);
       }
@@ -42,13 +41,20 @@ function set_9x9(str) {
   }
 }
 
-function draw_9x9() {
-  var s = '<table class="table">\n';
+function displayChangesInArray(str) {
+let nums = +str;
+console.log(nums)
 
-  for (var i = 0; i < 9; ++i) {
+}
+
+function draw_9x9() {
+  
+  let s = '<table class="table">\n';
+
+  for (let i = 0; i < 9; ++i) {
     s += '<tr>';
-    for (var j = 0; j < 9; ++j) {
-      var c = 'cell';
+    for (let j = 0; j < 9; ++j) {
+      let c = 'cell';
       if ((i + 1) % 3 == 0 && j % 3 == 0) {
         c = 'cell3';
       } else if ((i + 1) % 3 == 0) {
@@ -61,42 +67,45 @@ function draw_9x9() {
     s += '</tr>\n';
   }
 
-  
 
   s += '</table>';
   document.getElementById('9x9').innerHTML = s;
-  var inp = document.URL;
-  var set = false;
+  // let inp = document.URL;
+  // let set = false;
 
-  if (inp.indexOf('?') >= 0) {
-    var match = /[?&]puzzle=([^\s&]+)/.exec(inp);
-    if (match.length == 2 && match[1].length >= 81) {
-      set_9x9(match[1]);
-      set = true;
-    }
-  }
+  // if (inp.indexOf('?') >= 0) {
+  //   let match = /[?&]puzzle=([^\s&]+)/.exec(inp);
+  //   if (match.length == 2 && match[1].length >= 81) {
+  //     set_9x9(match[1]);
+  //     set = true;
+  //   }
+  // }
 
-  if (!set) {
-    set_9x9('001700509573024106800501002700295018009400305652800007465080071000159004908007053');
-  }
+  // if (!set) {
+  //   set_9x9('001700509573024106800501002700295018009400305652800007465080071000159004908007053');
+  // }
 
-  let initialArray = Array.from(document.querySelectorAll('td'))
+ setListeners()
+
+}
+
+function setListeners() {
+   let initialArray = Array.from(document.querySelectorAll('td'))
   initialArray.forEach(cell => {
-    cell.removeEventListener('keyup', listenSteps);
-    cell.addEventListener('keyup', listenSteps)
+    cell.removeEventListener('keyup', listenSteps, false);
+    cell.addEventListener('keyup', listenSteps, false);
   })
 }
 
 function listenSteps(e){
-  console.log(e.key)
-  let arr = []
-  arr.push(e.key)
-  console.log(arr)
-  document.getElementById('log').innerHTML += arr.join("\n")
+ stepCounter += 1;
+  array.push(e.key)
+  let stepstats = document.getElementById('log').innerHTML = array.join('\n')
+  
 }
 
 function clear_input() {
-  for (var i = 0; i < 81; ++i) {
+  for (let i = 0; i < 81; ++i) {
     document.getElementById('C' + i).value = '';
   }
 }
@@ -106,6 +115,8 @@ function setPredefined() {
       set_9x9('032054900090001004080700031005600027800070000270140005000210300018907652603000000');
       this.time = new Date().getTime();
       console.log(this.time);
+      array = [];
+      stepCounter = 0
       
 }
 
@@ -116,3 +127,6 @@ function timeCounter() {
     
 }
 
+// fs.writeFile('./stats.json', JSON.stringify(stepstats), err => {
+  // if (err) console.log(err);
+  // console.log(`${stepstats} stored.`)})
